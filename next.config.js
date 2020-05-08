3a26b9033e9b2078c3fdd82113c7fd9ed0780dd1
Brandon <brandongines@gmail.com>
2020-05-08 03:14:41 -0400

    deploy-net

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ require("dotenv").config();
 module.exports = {
   env: {
     PUBLISHABLE_KEY: process.env.PUBLISHABLE_KEY
-  }
+  },
+  target: "serverless",
+  // distDir already has .next as default
+  distDir: ".next"
 };
